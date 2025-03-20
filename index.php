cb982e11c72fd3fbecb05700df0c59b860b12243
Dimas Agil S. W. <92960385+projectdasw@users.noreply.github.com>
2025-03-20 15:27:35 +0700

    Initial File

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Check In - QR Code</title>
+        <!-- CSS CORE -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+            rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+            crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/ee5974d453.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container border border-1">
+            <h1 class="text-center p-2 mb-3">Absensi Kehadiran Tamu Acara</h1>
+            <div class="row justify-content-evenly">
+                <div class="col-5 border border-1">
+                    <div class="input-group mb-3">
+                        <input type="search" class="form-control" placeholder="Cari Tamu Acara"
+                            aria-label="Cari Tamu Acara" aria-describedby="button-addon2">
+                        <button class="btn btn-outline-success" type="button" id="button-addon2">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                            <span class="p-1">Cari Tamu</span>
+                        </button>
+                    </div>
+                    <div class="card">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="assets/tamu-img.jpg" class="img-fluid rounded-start" alt="image">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-5 border border-1">
+                    One of two columns
+                </div>
+            </div>
+        </div>
+        <!-- JS CORE -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"></script>
+    </body>
+</html>
